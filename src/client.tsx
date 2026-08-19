ac334663e3021c59b136a073a93e344ed4b19d01
znamteam-max <znamteam@gmail.com>
2026-08-19 08:46:27 +0300

    Load Vsesport logo preview override

--- src/client.tsx
+++ src/client.tsx
@@ -4,6 +4,7 @@ import App from './AppV2';
 import './styles.css';
 import './sponsor-exact.css';
 import './v2.css';
+import './v2-brand.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
