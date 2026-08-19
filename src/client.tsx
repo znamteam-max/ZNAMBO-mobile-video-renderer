eb31366ce89344b1fe847529dd391b191bafdd18
znamteam-max <znamteam@gmail.com>
2026-08-19 08:44:09 +0300

    Switch app to V2 editor

--- src/client.tsx
+++ src/client.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './AppV2';
 import './styles.css';
 import './sponsor-exact.css';
+import './v2.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
